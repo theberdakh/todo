45cb1eb361af3f489ee30e98f532584e1610d1b9
thebe <theberdakh@gmail.com>
2025-03-02 07:07:08 +0500

    init

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,15 @@
+package com.theberdakh
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
